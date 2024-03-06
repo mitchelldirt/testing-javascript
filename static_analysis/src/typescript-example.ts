1314296c7098ccec2f320de234913246b8334bc2
Mitchell Mudd <developer@mitchellmudd.dev>
2024-03-06 10:58:45 -0500

    Testing husky

--- static_analysis/src/typescript-example.ts
+++ static_analysis/src/typescript-example.ts
@@ -1,21 +1,22 @@
-function add(a: number, b: number): number {
-  return a + b
-}
-
-interface User {
-  name: {
-    first: string
-    middle: string
-    last: string
+function add (a: number, b: number): number {
+    return a + b
   }
-}
-function getFullName(user: User): string {
-  const {
-    name: {first, middle, last},
-  } = user
-  return [first, middle, last].filter(Boolean).join('')
-}
 
-add(1, 2)
-
-getFullName({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}})
+  interface User {
+    name: {
+      first: string
+      middle?: string
+      last: string
+    }
+  }
+  
+  function getFullName(user: User): string {
+    const {
+      name: {first, middle, last},
+    } = user
+    return [first, middle, last].filter(Boolean).join('')
+  }
+  
+  add(1, 2)
+  
+  getFullName({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}})
