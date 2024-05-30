d684e7a4cd9cf258a72c239d57dc3d33d2d7d8b0
Mitchell Mudd <developer@mitchellmudd.dev>
2024-05-30 09:10:45 -0400

    Learned about test object factories and defensive testing.

--- testing-node-apps-tjs/src/utils/__tests__/error-middleware.exercise.js
+++ testing-node-apps-tjs/src/utils/__tests__/error-middleware.exercise.js
@@ -1,24 +1,30 @@
 // Testing Middleware
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
-
 // 🐨 you'll need both of these:
 import {UnauthorizedError} from 'express-jwt'
 import errorMiddleware from '../error-middleware'
+
+function buildRes(overrides) {
+  const res = {
+    json: jest.fn(() => res),
+    status: jest.fn(() => res),
+    ...overrides,
+  }
+  return res
+}
 
 // 🐨 Write a test for the UnauthorizedError case
 // 💰 const error = new UnauthorizedError('some_error_code', {message: 'Some message'})
 // 💰 const res = {json: jest.fn(() => res), status: jest.fn(() => res)}
 describe('Unauthorized Error', () => {
   it('Gives me a 401 error', () => {
-    const error = new UnauthorizedError('Unauthorized', {
-      message: 'You cannot access this',
-    })
+    const code = 'some_error_code'
+    const message = 'Some message'
+    const error = new UnauthorizedError(code, {message: message})
 
     const req = {}
     const next = jest.fn()
-    const res = {json: jest.fn(() => res), status: jest.fn(() => res)}
+    const res = buildRes()
     res.headersSent = false
 
     // we're passing res to this which it then "modifies" which is why it doesn't need to be within a variable
@@ -32,24 +38,26 @@ describe('Unauthorized Error', () => {
       code: error.code,
       message: error.message,
     })
+
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
 
 // 🐨 Write a test for the headersSent case
 describe('headersSent Case', () => {
   it('calls next if headers were sent', () => {
-    const error = new Error('Blah')
     const req = {}
-    const res = {
-      json: jest.fn(() => res),
-      status: jest.fn(() => res),
-      headersSent: true,
-    }
+    const res = buildRes({headersSent: true})
     const next = jest.fn()
 
+    const error = new Error('Blah')
     errorMiddleware(error, req, res, next)
 
     expect(next).toHaveBeenCalledWith(error)
+    expect(next).toHaveBeenCalledTimes(1)
+    expect(res.status).not.toHaveBeenCalled()
+    expect(res.json).not.toHaveBeenCalled()
   })
 
   it('does not call next because headersSent is false', () => {
@@ -69,3 +77,22 @@ describe('headersSent Case', () => {
 })
 
 // 🐨 Write a test for the else case (responds with a 500)
+describe('Else case', () => {
+  it('Should return a 500 error and the error object', () => {
+    const error = new Error('Blah')
+    const req = {}
+    const res = buildRes()
+    const next = jest.fn()
+
+    errorMiddleware(error, req, res, next)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({
+      message: error.message,
+      stack: error.stack,
+    })
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(next).not.toHaveBeenCalled()
+  })
+})
